d6d7132fdac63856979816dae436a5e662c4976b
Nemanja Velimirovic <velimirovicnemanja@yahoo.com>
2021-08-19 11:41:56 -0700

    Initial Commit

---
+++ LuckySlotMachine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LuckySlotMachine
+//
+//  Created by xc51b8 on 2021-08-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LuckySlotMachine/LuckySlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckySlotMachineApp.swift
+//  LuckySlotMachine
+//
+//  Created by xc51b8 on 2021-08-19.
+//
+
+import SwiftUI
+
+@main
+struct LuckySlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
